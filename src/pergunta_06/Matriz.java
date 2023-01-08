f2b35fa6f343b207c658ce985cc74e0b513dc8de
IvanNogueira <ivanrezendenogueira@hotmail.com>
2023-01-08 05:06:23 -0300

    exercicio 6 atualização

--- src/pergunta_06/Matriz.java
+++ src/pergunta_06/Matriz.java
@@ -14,48 +14,48 @@ import java.util.Scanner;
  */
 public class Matriz {
     int tamanho;
-    int [][]matriz= new int[3][3];
-    int []aux = new int[3];
+    int [][]matriz= new int[numero][numero];
+    int []aux = new int[numero];
     int i, c,t;
     public static int numero;
-    
+
     public static final String ANSI_RED = "\u001B[31m";   
     public static void main(){
-        
+
          Scanner entrada = new Scanner(System.in);
          System.out.print("Qual tamanho da matriz que deseja:");
          numero = entrada.nextInt();
         Matriz matriz;
         matriz = new Matriz();
-        
+
         matriz.geradorMatriz();
         matriz.imprimirMatriz();
         matriz.diagonalPricipal();
         matriz.diagonalSecundaria();
         System.out.println(ANSI_RED+"Matriz com elementos da diagonal Principal inverta na diagonal Secundária ");
         matriz.imprimirMatriz();
-        
-   
+
+
     }//fechamento do pergunta 06
-  
-     
-        
-              
+
+
+
+
        public void geradorMatriz(){
         //preencher a matriz gerando valores aleatórios
          tamanho=numero;
          System.out.println("Gerando valores aleatórios da matriz.");
-         
+
         for(i = 0; i < tamanho; i++ ){
             for(c = 0; c < tamanho;c++){
                 matriz [i][c] = (int)(Math.random()*10); ;
             }
         }
        }
-       
+
        //imprimir matriz
        public void imprimirMatriz(){
-        
+
         for(i = 0; i < tamanho; i++ ){
             for(c = 0; c < tamanho;c++){
                 System.out.print(matriz [i][c] + " ");
@@ -63,7 +63,7 @@ public class Matriz {
             System.out.println("");
         }
        }
-       
+
        //imprimir diagonal principal e armazenando valores no auxiliar
        public void diagonalPricipal(){
          System.out.print("Diagonal Principal: ");
@@ -74,7 +74,7 @@ public class Matriz {
          System.out.println("");
          System.out.println("Diagonal principal invertida: "+Arrays.toString(aux));
        }
-       
+
         //imprimir diagonal secundaria e aproveitando codigo para fazer a troca com auxiliar
        public void diagonalSecundaria(){
             System.out.print("Diagonal Secundaria: ");
---
+++ src/pergunta_07/padrao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pergunta_07;
+
+/**
+ *
+ * @author ivan
+ */
+public class padrao {
+    
+    public static void Main(string[] args) {
+            CriaMatriz("A.txt",200,300);
+            
+        }
+    
+}
